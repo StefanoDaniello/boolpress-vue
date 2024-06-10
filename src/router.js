2010f4574cf106d5064ec26d97ef98b1dba556bd
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-06-10 15:48:44 +0200

    start

---
+++ src/router.js
@@ -0,0 +1,17 @@
+// import { createRouter, createWebHistory } from 'vue-router'
+// import AppHome from './pages/Home.vueconst router = createRouter({
+//     history: createWebHistory(import.meta.env.BASE_URL),
+//     routes: [
+//         {
+//             path: '/',
+//             name: 'home',
+//             component: AppHome
+//         },
+//         {
+//             path: '/posts',
+//             name: 'posts',
+//             component: PostList
+//         },  
+//     ]
+// })
+// export default router
