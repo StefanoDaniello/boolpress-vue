c7930a031ebd5ab1f028583412a80ea55ce47583
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-06-10 15:48:56 +0200

    add posts

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,41 @@
 <template>
-  <h1 class="text-center">Hello</h1>
-  <i class="fa-solid fa-house"></i>
+  <h1 class="text-center">Ciao</h1>
+  <i class="fa fa-solid fa-home"></i>
+  <ul>
+    <li v-for="post in posts" :key="post.id">{{ post.title }}
+      <img :src="store.imgBasePath + post.image" :alt="post.title">
+    </li>
+  </ul>
+  <button @click="getAllPosts()" class="btn btn-primary">cambia pagina</button>
 </template>
 
 <script>
-  export default {
-    name: 'App',
+import { store } from './store';
+import axios from 'axios';
+export default {
+  name: 'App',
+  data() {
+    return {
+      store,
+      posts: [],
+
+    }
+  },
+  methods: {
+    getAllPosts() {
+      axios.get(this.store.apiBaseUrl + '/posts').then((res) => {
+        console.log(res.data);
+        this.posts = res.data.results;
+        //se paginazione
+        //this.posts = res.data.results.data;
+        //this.currentPage = res.data.results.current_page;
+      });
+    },
+  },
+  mounted() {
+    this.getAllPosts();
   }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
